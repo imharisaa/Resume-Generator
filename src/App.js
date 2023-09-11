4202332e5e2d460f6f3c47d4cfee090610d5ecec
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2023-09-11 15:03:18 +0300

    theme implementation complete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import './App.css';
-import { AppShell } from '@mantine/core';
-import Toolbar from './components/Header/Toolbar';
+import "./App.css";
+import { AppShell, MantineProvider } from "@mantine/core";
+import Toolbar from "./components/Header/Toolbar";
 
-import {
-  Route,
-  Routes
-} from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import Resume from './Pages/Resume/Resume';
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Resume from "./Pages/Resume/Resume";
+import { colorPlates } from "./Theming/CustomColorPlates/Plates";
 
 function App() {
   return (
     <div className="App">
-        <AppShell
-          header={<Toolbar />}
-        >
-
-        <Routes>
-          {/* <Route exact path='/' element={<Home />}  /> */}
-          {/* <Route exact path="/resume-editor" element={<Resume />} /> */}
-          <Route exact path="/" element={<Resume />} />
-        </Routes>
-          
+      <MantineProvider theme={{
+        colorScheme: 'light',
+        colors: {
+          custom: colorPlates.blue
+        }
+      }} >
+        <AppShell header={<Toolbar />}>
+          <Routes>
+            {/* <Route exact path='/' element={<Home />}  /> */}
+            {/* <Route exact path="/resume-editor" element={<Resume />} /> */}
+            <Route exact path="/" element={<Resume />} />
+          </Routes>
         </AppShell>
+      </MantineProvider>
     </div>
   );
 }
 
 export default App;
+
