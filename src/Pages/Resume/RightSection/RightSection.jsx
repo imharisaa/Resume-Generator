4ff74845fb510da42787fa760bd3c1556f4cab50
romesa.dev <dev.romesa@gmail.com>
2023-09-18 16:36:43 +0300

    Third Template complete. to open drawer, arrow button added

--- src/Pages/Resume/RightSection/RightSection.jsx
+++ src/Pages/Resume/RightSection/RightSection.jsx
@@ -1,28 +1,45 @@
-import { Box, Container, Flex, Grid, MantineProvider, useMantineTheme } from "@mantine/core";
+import {
+  Box,
+  Container,
+  Flex,
+  Grid,
+  MantineProvider,
+  useMantineTheme,
+} from "@mantine/core";
 import React from "react";
 import useRightSectionStyle from "./RightSection.style";
 import FristTemplate from "../../../Resume_Preview/Templates/First.Template/First.template";
 import { useSelector } from "react-redux";
 import { colorPlates } from "../../../Theming/CustomColorPlates/Plates";
 import SecondTemplate from "../../../Resume_Preview/Templates/Second.Template/Second.template";
-
+import ThirdTemplate from "../../../Resume_Preview/Templates/third-Template/Third.template";
 
 const RightSection = () => {
-  const themeData = useSelector((state) => state.theme.colorPlate)
-  const { classes: {right_section_container} } = useRightSectionStyle()
+  const themeData = useSelector((state) => state.theme.colorPlate);
+  const {
+    classes: { right_section_container },
+  } = useRightSectionStyle();
   const theme = useMantineTheme();
   return (
     <>
-      <Container className={right_section_container} p={"xl"} h={"95vh"} w={"100vw"}>
+      <Container
+        className={right_section_container}
+        p={"xl"}
+        h={"95vh"}
+        w={"100vw"}
+      >
         <Grid gutter={"lg"}>
-          <Flex justify={'center'} align={'center'} w={'100%'} h={'100%'}>
-            <MantineProvider theme={{
+          <Flex justify={"center"} align={"center"} w={"100%"} h={"100%"}>
+            <MantineProvider
+              theme={{
                 colors: {
-                  custom: colorPlates.blue
-                }
-            }}>
+                  custom: colorPlates.blue,
+                },
+              }}
+            >
               {/* <FristTemplate /> */}
-              <SecondTemplate />
+              {/* <SecondTemplate /> */}
+              <ThirdTemplate />
             </MantineProvider>
           </Flex>
         </Grid>
