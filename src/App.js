2624fef45f595b63497c3329e07c41c998bde2be
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2023-09-24 15:45:38 +0300

    Stripe Payment Module Is Implementated But Not Working With Original Account Cuz Need To Open LLC For Activate The Stripe Account

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
-import { AppShell, MantineProvider } from "@mantine/core";
-import Toolbar from "./components/Header/Toolbar";
 
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import StripeContainer from "./Pages/Payment/StripeContainer";
 import Resume from "./Pages/Resume/Resume";
 import { colorPlates } from "./Theming/CustomColorPlates/Plates";
+import Payment from "./Pages/Payment/Payment";
+import Success from "./Pages/Success/Success";
+import Cancel from "./Pages/Cancel/Cancel";
 
 function App() {
   return (
     <div className="App">
-      <MantineProvider theme={{
-        colorScheme: 'light',
-        colors: {
-          custom: colorPlates.blue
-        }
-      }} >
-        <AppShell header={<Toolbar />}>
+      <MantineProvider
+        theme={{
+          colorScheme: "light",
+          colors: {
+            custom: colorPlates.blue,
+          },
+        }}
+      >
           <Routes>
-            {/* <Route exact path='/' element={<Home />}  /> */}
-            {/* <Route exact path="/resume-editor" element={<Resume />} /> */}
-            <Route exact path="/" element={<Resume />} />
+            {/* <Route exact path="/payment" element={<StripeContainer />} /> */}
+            <Route exact path="/payment" element={<Payment />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/resume" element={<Resume />} />
+            <Route exact path="/success" element={<Success />} />
+            <Route exact path="/cancel" element={<Cancel />} />
           </Routes>
-        </AppShell>
       </MantineProvider>
     </div>
   );
 }
 
 export default App;
-
--- src/store/forms.reducer.js
+++ src/store/forms.reducer.js
@@ -20,7 +20,7 @@ const formsSlice = createSlice({
       dateOfBirth: "",
     },
     professionalSummary: {
-      details: `Motivated Sales Associate with 5 years of experience boosting sales and customer loyalty through individualized service. Resourceful expert at learning customer needs, directing to desirable merchandise and upselling to meet sales quotas. Committed to strengthening customer experiences with positivity and professionalism when answering requests and processing sales.`,
+      details: `<p>Motivated Sales Associate with 5 years of experience boosting sales and customer loyalty through individualized service. Resourceful expert at learning customer needs, directing to desirable merchandise and upselling to meet sales quotas. Committed to strengthening customer experiences with positivity and professionalism when answering requests and processing sales.</p>`,
     },
     employmentHistory: [],
     education: [],
